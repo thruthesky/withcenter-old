a611fc6a95fcbfa1f35d56d499c8ee93fc43ba67
JaeHo Song <thruthesky@gmail.com>
2015-07-06 23:23:13 +0900

    work for sonub module

--- templates/mall_philgo/init.php
+++ templates/mall_philgo/init.php
@@ -1,34 +1,7 @@
 <?php
-// di("hello, what am i? : hook=$hook");
-
-
 use Drupal\post\Entity\PostData;
-
-
-
-$where = PostData::getQueryOnConds([]); // return null
-
-$where = PostData::getQueryOnConds([
-    'post_config_name' => 'job',
-]); // returns "WHERE config_id=2 AND parent_id=0"
-$where = PostData::getQueryOnConds([
-    'post_config_name' => 'job',
-    'original_only' => true
-]); // returns "WHERE config_id=2 AND parent_id=0"
-$where = PostData::getQueryOnConds([
-    'post_config_name' => 'job',
-    'original_only' => false
-]); // WHERE config_id=2
-
-PostData::search([
-    'post_config_name' => 'job',
-    'original_only' => false,
-    'order_field' => 'created',
-    'order_direction' => 'DESC',
-]); // SQL : SELECT `id` FROM post_data WHERE config_id=2 ORDER BY `created` DESC LIMIT 0,10
-
-
-PostData::search([
+$posts = PostData::search([
     'post_config_name' => 'job',
     'fid_of_first_image' => true
-]); // SQL : SELECT `id` FROM post_data WHERE config_id=2 AND parent_id=0 AND fid_of_first_image>0 ORDER BY `id` DESC LIMIT 0,10
+]);
+$variables['data']['latest_with_photo'] = $posts;
--- templates/mall_sonub/init.php
+++ templates/mall_sonub/init.php
@@ -1,35 +1,6 @@
 <?php
 use Drupal\post\Entity\PostData;
 
-
-/*
-$where = PostData::getQueryOnConds([]); // return null
-
-$where = PostData::getQueryOnConds([
-    'post_config_name' => 'job',
-]); // returns "WHERE config_id=2 AND parent_id=0"
-$where = PostData::getQueryOnConds([
-    'post_config_name' => 'job',
-    'original_only' => true
-]); // returns "WHERE config_id=2 AND parent_id=0"
-$where = PostData::getQueryOnConds([
-    'post_config_name' => 'job',
-    'original_only' => false
-]); // WHERE config_id=2
-
-PostData::search([
-    'post_config_name' => 'job',
-    'original_only' => false,
-    'order_field' => 'created',
-    'order_direction' => 'DESC',
-]); // SQL : SELECT `id` FROM post_data WHERE config_id=2 ORDER BY `created` DESC LIMIT 0,10
-
-
-PostData::search([
-    'post_config_name' => 'job',
-    'fid_of_first_image' => true
-]); // SQL : SELECT `id` FROM post_data WHERE config_id=2 AND parent_id=0 AND fid_of_first_image>0 ORDER BY `id` DESC LIMIT 0,10
-*/
 if( $hook == 'page' ){
 	$post_with_thumbnail = PostData::search([
 										'post_config_name' => 'job',
