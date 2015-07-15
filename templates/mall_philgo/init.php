a357b0cb6dae3a85abed75f6ba542295cc5d7a25
JaeHo Song <thruthesky@gmail.com>
2015-07-16 00:57:29 +0900

    working for drupalkorea

--- templates/mall_philgo/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Drupal\post\Entity\PostData;
-$posts = PostData::search([
-    'post_config_name' => 'job',
-    'fid_of_first_image' => true
-]);
-$variables['data']['latest_with_photo'] = $posts;
