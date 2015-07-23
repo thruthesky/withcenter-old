e6f360dc8dc11a79b3326eec1c8874bc48cc4811
mark <mark@gmail.com>
2015-07-23 06:47:57 -0400

    fixes for member profile box

--- js/default.js
+++ js/default.js
@@ -13,11 +13,19 @@ $(function(){
 	$body.on('mouseleave', selector_member_profile_box, on_member_profile_leave);
 	
     function on_member_profile(e) {
-		clearTimeout( member_profile_timeout );
-		//$(selector_member_profile_box).remove();
+		clearTimeout( member_profile_timeout );				
 		
 		var uid = $(this).attr('uid');
         var target_id = $(this).attr('target_id');
+		
+		if( $(".member-profile-box[uid='"+uid+"'][target_id='"+target_id+"']").length ){
+		
+		}
+		else{
+			$(selector_member_profile_box).remove();
+		}
+		
+		
 		var url = "/library/api?call=getMemberProfile&uid=" + uid + "&target_id=" + target_id;
 		ajax_api(url, callback_show_member_profile);
 		/*
@@ -50,23 +58,17 @@ $(function(){
     }
 	
 	function callback_show_member_profile( re ){
-		console.log( re );
 		$body.append( re.markup );
-		//console.log( selector_member_profile_box );
 		var h_member_profile = $('.member-profile').outerHeight() + 15;
 		var position = $(".member-profile[uid='"+re.uid+"'][target_id='"+re.target_id+"']").position();
         var top = position.top + h_member_profile;
         var left = position.left + 10;
-		
-		console.log( left );
 		
 		
 		$(".member-profile-box").css({
             'position': 'absolute',
             'top': top + 'px',
             'left': left + 'px',
-            //'padding': '1em',
-            //'background-color': '#f2f2f2'
         });
 	}
 	
@@ -81,7 +83,7 @@ $(function(){
 		clearTimeout( member_profile_timeout );
 	}
 	/*EO on_member_box_profile*/
-	
+	/*
     function get_member_profile_box(uid, name) {
         var m = "<div class='member-profile-box'>";
 		m += "<div class='triangle'></div>";
@@ -92,6 +94,6 @@ $(function(){
         m += "</div>";
         return m;
     }
-
+	*/
 });
 
